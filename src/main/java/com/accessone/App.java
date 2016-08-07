bc385d9fee8664946f4f54a41ed6afdc38ab6f3f
Duncan Browne <duncbrowne@gmail.com>
2016-08-07 14:30:02 +0100

    Created singleton class for composing JSON messages and added an example.

--- src/main/java/com/accessone/App.java
+++ src/main/java/com/accessone/App.java
@@ -13,30 +13,36 @@ import java.util.HashMap;
 import com.accessone.Database.MariaDBFacade;
 import com.accessone.Messages.RabbitMQEventMessageSender;
 import com.accessone.Messages.RabbitMQRequestMessageReceiver;
+import com.accessone.Messages.composer.GetCardholderMessageComposer;
 
 public class App 
 {
 
     public static void main( String[] args ) throws IOException, TimeoutException, SQLException
     {
-        MariaDBFacade dbFacade = new MariaDBFacade();
+        /*MariaDBFacade dbFacade = new MariaDBFacade();
         dbFacade.GetCardholderRecord(1, null);
-        dbFacade.GetCardholderRecord(4, null);
+        dbFacade.GetCardholderRecord(4, null);*/
+
+        GetCardholderMessageComposer composer = GetCardholderMessageComposer.getInstance();
+        String strMessage = composer.createGetCardholderByCardholderIDMessage(2);
+        assert strMessage == "{\"requester\":{\"id\":111,\"type\":\"automatedtester\"},\"data\":{\"get\":{\"type\":\"cardholder\",\"cardholderid\":\"2\"}}}";
+        System.out.println(strMessage);
 
          // Set up the RabbitMQ Request message receiver
-        RabbitMQRequestMessageReceiver consumer = new RabbitMQRequestMessageReceiver("queue");
+        RabbitMQRequestMessageReceiver consumer = new RabbitMQRequestMessageReceiver("response");
         Thread consumerThread = new Thread(consumer);
         consumerThread.start();
 
         // Setup the RabbitMQ Event message sender
-        RabbitMQEventMessageSender producer = new RabbitMQEventMessageSender("queue");
+        RabbitMQEventMessageSender producer = new RabbitMQEventMessageSender("request");
 
-        for (int i = 0; i < 10; i++) {
+        /*for (int i = 0; i < 10; i++) {
             HashMap message = new HashMap();
             message.put("message number", i);
             producer.sendMessage(message);
             System.out.println("Message Number "+ i +" sent.");
-        }
+        }*/
 
     }
 }
---
+++ src/main/java/com/accessone/Messages/composer/GetCardholderMessageComposer.java
@@ -0,0 +1,43 @@
+package com.accessone.Messages.composer;
+
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+
+/**
+ * Current Project cardholders_tester
+ * Created by duncan.browne on 07/08/2016.
+ */
+public class GetCardholderMessageComposer
+{
+    private static GetCardholderMessageComposer ourInstance = new GetCardholderMessageComposer();
+
+    public static GetCardholderMessageComposer getInstance()
+    {
+        return ourInstance;
+    }
+
+    private static int REQUESTER_ID = 111;
+    private static String REQUESTER_TYPE = "automatedtester";
+
+    private GetCardholderMessageComposer()
+    {
+    }
+
+    public String createGetCardholderByCardholderIDMessage(int cardholderID)
+    {
+        JsonBuilderFactory factory = Json.createBuilderFactory(null);
+        JsonObject value = factory.createObjectBuilder()
+                .add("requester", factory.createObjectBuilder()
+                        .add("id", REQUESTER_ID)
+                        .add("type", REQUESTER_TYPE))
+                .add("data", factory.createObjectBuilder()
+                        .add("get", factory.createObjectBuilder()
+                            .add("type", "cardholder")
+                            .add("cardholderid", Integer.toString(cardholderID))))
+                .build();
+
+        String strJSONMsg = value.toString();
+        return strJSONMsg;
+    }
+}
